b8eb76dc1796727cc4e0ab461f18b8b237bc4074
Spencer Carli <spencercarli@gmail.com>
2018-04-03 10:03:32 -0500

    Navigator improvements

--- mobile/App/components/Navigator.js
+++ mobile/App/components/Navigator.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { View, Animated, Dimensions } from "react-native";
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
+
+export const DIRECTIONS = {
+  horizontal: "horizontal",
+  vertical: "vertical",
+};
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,39 +15,59 @@ export default class App extends React.Component {
     this.state = {
       currentScene: props.initialSceneName || Object.keys(props.scenes)[0],
       nextSceneProps: null,
+      direction: DIRECTIONS.horizontal,
     };
   }
 
   componentDidMount() {
-    this.display();
+    this.animateScreenForward(false);
   }
 
   animatedValue = new Animated.Value(0);
 
-  display = () =>
+  animateScreenForward = dismiss =>
     new Promise((resolve) => {
       Animated.spring(this.animatedValue, {
-        toValue: 1,
-        duration: 500,
-        useNativeDriver: true,
-      }).start(() => resolve());
-    });
-
-  dismiss = () =>
-    new Promise((resolve) => {
-      Animated.spring(this.animatedValue, {
-        toValue: 2,
+        toValue: dismiss ? 2 : 1,
         duration: 500,
         useNativeDriver: true,
       }).start(() => {
-        this.animatedValue.setValue(0);
+        if (dismiss) {
+          this.animatedValue.setValue(0);
+        }
         resolve();
       });
     });
 
-  goTo = (sceneName, props = {}) => {
-    this.dismiss().then(() => {
-      this.setState({ currentScene: sceneName, nextSceneProps: props }, () => this.display());
+  animateScreenBackward = dismiss =>
+    new Promise((resolve) => {
+      Animated.spring(this.animatedValue, {
+        toValue: dismiss ? 0 : 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start(() => {
+        if (dismiss) {
+          this.animatedValue.setValue(2);
+        }
+        resolve();
+      });
+    });
+
+  goTo = (sceneName, props = {}, direction = DIRECTIONS.horizontal) => {
+    this.setState({ direction }, () => {
+      this.animateScreenForward(true).then(() => {
+        this.setState({ currentScene: sceneName, nextSceneProps: props }, () =>
+          this.animateScreenForward(false));
+      });
+    });
+  };
+
+  goBackTo = (sceneName, props = {}, direction = DIRECTIONS.horizontal) => {
+    this.setState({ direction }, () => {
+      this.animateScreenBackward(true).then(() => {
+        this.setState({ currentScene: sceneName, nextSceneProps: props }, () =>
+          this.animateScreenBackward(false));
+      });
     });
   };
 
@@ -50,22 +75,34 @@ export default class App extends React.Component {
     const scene = this.props.scenes[this.state.currentScene];
     const CurrentScene = scene ? scene.component : View;
 
-    const translateX = this.animatedValue.interpolate({
-      inputRange: [0, 1, 2],
-      outputRange: [width, 0, -width],
-    });
+    const s = {
+      alignSelf: "stretch",
+      flex: 1,
+    };
 
-    const s = [
-      {
-        alignSelf: "stretch",
-        flex: 1,
-        transform: [{ translateX }],
-      },
-    ];
+    if (this.state.direction === DIRECTIONS.horizontal) {
+      s.transform = [
+        {
+          translateX: this.animatedValue.interpolate({
+            inputRange: [0, 1, 2],
+            outputRange: [width, 0, -width],
+          }),
+        },
+      ];
+    } else if (this.state.direction === DIRECTIONS.vertical) {
+      s.transform = [
+        {
+          translateY: this.animatedValue.interpolate({
+            inputRange: [0, 1, 2],
+            outputRange: [height, 0, -height],
+          }),
+        },
+      ];
+    }
 
     return (
       <Animated.View style={s}>
-        <CurrentScene goTo={this.goTo} {...this.state.nextSceneProps} />
+        <CurrentScene goTo={this.goTo} goBackTo={this.goBackTo} {...this.state.nextSceneProps} />
       </Animated.View>
     );
   }
